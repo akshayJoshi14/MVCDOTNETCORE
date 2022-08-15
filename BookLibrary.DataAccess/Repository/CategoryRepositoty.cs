c36a0878d2fbeb7c6a61af3a0efab75e6be558b6
akki14192 <ajoshi@odysseussolutions.com>
2022-08-15 16:04:45 -0400

    Implemented category repository.

---
+++ BookLibrary.DataAccess/Repository/CategoryRepositoty.cs
@@ -0,0 +1,30 @@
+﻿using BookLibrary.DataAccess.Data;
+using BookLibrary.DataAccess.Repository.IRepository;
+using BookLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLibrary.DataAccess.Repository
+{
+    public class CategoryRepositoty : Repositary<Category>, ICategoryRepository
+    {
+        private ApplicationDbContext _db;
+
+        public CategoryRepositoty(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Categories.Update(obj);
+        }
+    }
+}
