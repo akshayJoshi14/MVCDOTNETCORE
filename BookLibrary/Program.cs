b56dee41fe3e5cadf7f7586a12f5acf43ba3f279
akki14192 <ajoshi@odysseussolutions.com>
2022-09-18 05:59:28 -0400

    Add migration for entity table.

--- BookLibrary/Program.cs
+++ BookLibrary/Program.cs
@@ -38,6 +38,8 @@ app.UseAuthentication();
 
 app.UseAuthorization();
 
+app.MapRazorPages();
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
