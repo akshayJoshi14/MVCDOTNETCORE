ed26cc49a9f0abdb33804c119985d6d84b012a8c
akki14192 <ajoshi@odysseussolutions.com>
2022-09-18 08:55:20 -0400

    Added User Role feature where we can assign diffrent types of rules for user.

--- BookLibrary/Program.cs
+++ BookLibrary/Program.cs
@@ -3,6 +3,8 @@ using BookLibrary.DataAccess.Repository;
 using BookLibrary.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using BookLibrary.Utility;
+using Microsoft.AspNetCore.Identity.UI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,10 +14,12 @@ builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlSer
      builder.Configuration.GetConnectionString("DefaultConnection")
     ));
 
-builder.Services.AddDefaultIdentity<IdentityUser>()
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddDefaultTokenProviders()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+builder.Services.AddSingleton<IEmailSender, EmailSender>();
 
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
