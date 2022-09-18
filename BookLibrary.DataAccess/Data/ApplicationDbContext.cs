6829249cfeb4d6328c3d48c1715de8c72edfc33b
akki14192 <ajoshi@odysseussolutions.com>
2022-09-18 06:22:07 -0400

    Added few more preoprty like name, city, adrress on user.

--- BookLibrary.DataAccess/Data/ApplicationDbContext.cs
+++ BookLibrary.DataAccess/Data/ApplicationDbContext.cs
@@ -16,5 +16,7 @@ namespace BookLibrary.DataAccess.Data
         public DbSet<CoverType> CoverTypes { get; set; }
 
         public DbSet<Product> Products { get; set; }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     }
 }
